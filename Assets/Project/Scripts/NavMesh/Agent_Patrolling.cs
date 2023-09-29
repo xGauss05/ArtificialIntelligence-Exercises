d38bca97915afebef7b40e60b3fc361c0d2413d0
Jonathan Cacay Llanes <57706173+xGauss05@users.noreply.github.com>
2023-09-29 09:50:29 +0200

    Update Agent_Patrolling.cs
    Attempt to implement backwards patrolling.

--- Assets/Project/Scripts/NavMesh/Agent_Patrolling.cs
+++ Assets/Project/Scripts/NavMesh/Agent_Patrolling.cs
@@ -3,29 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class Agent_Patrolling : MonoBehaviour
-{
+public class Agent_Patrolling :MonoBehaviour {
     [SerializeField] NavMeshAgent agent;
     [SerializeField] GameObject[] waypoints;
     int patrolWP = 0;
-
-    void Start() 
-    {
+    bool backwards;
+    void Start() {
         patrolWP = Random.Range(0,waypoints.Length - 1);
+
+        int alea = Random.Range(1,2);
+        if (alea % 2 == 0) {
+            backwards = true;
+        } else {
+            backwards = false;
+        }
     }
 
-    void Update()
-    {
+    void Update() {
         if (!agent.pathPending && agent.remainingDistance < 0.3f) Patrol();
     }
 
-    void Patrol()
-    {
+    void Patrol() {
         if (waypoints.Length == 0)
             return;
 
         agent.destination = waypoints[patrolWP].transform.position;
-        patrolWP = (patrolWP + 1) % waypoints.Length;
-        
+        if (!backwards) {
+            patrolWP = (patrolWP + 1) % waypoints.Length;
+        } else {
+            patrolWP--;
+            if (patrolWP < 0) {
+                patrolWP = waypoints.Length - 1;
+            }
+        }
+
+
     }
 }
