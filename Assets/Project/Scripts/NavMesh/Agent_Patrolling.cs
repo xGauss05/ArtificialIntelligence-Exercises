497ade66df7e3cc04d1cb33935a8ecfb283ee8d5
Ethanm-0371 <99729919+Ethanm-0371@users.noreply.github.com>
2023-09-29 11:35:10 +0200

    Implemented automatic waypoint initialization.
    Also modified ghost NavMeshAgent.

--- Assets/Project/Scripts/NavMesh/Agent_Patrolling.cs
+++ Assets/Project/Scripts/NavMesh/Agent_Patrolling.cs
@@ -9,27 +9,25 @@ public class Agent_Patrolling :MonoBehaviour {
 
     [SerializeField] NavMeshAgent agent;
     [SerializeField] NavMeshAgent ghost;
+    [SerializeField] GameObject waypointsParent;
     [SerializeField] GameObject[] waypoints;
     [SerializeField] bool backwards;
     int patrolWP = 0;
 
 
     void Start() {
-        patrolWP = Random.Range(0,waypoints.Length - 1);
 
-        int alea = Random.Range(0,2);
-        if (alea % 2 == 0) {
-            backwards = true;
-        } else {
-            backwards = false;
-        }
+        InitWaypoints();
+        StartingPointDir();
     }
 
     void Update() {
+
         if (!ghost.pathPending && ghost.remainingDistance < 0.3f) Patrol();
 
         agent.destination = ghost.gameObject.transform.position;
     }
+
 
     void Patrol() {
         if (waypoints.Length == 0)
@@ -43,6 +41,30 @@ public class Agent_Patrolling :MonoBehaviour {
             if (patrolWP < 0) {
                 patrolWP = waypoints.Length - 1;
             }
+        }
+    }
+
+    void InitWaypoints()
+    {
+        waypoints = new GameObject[waypointsParent.transform.childCount];
+
+        for (int childIndex = 0; childIndex < waypointsParent.transform.childCount; childIndex++)
+        {
+            waypoints[childIndex] = waypointsParent.transform.GetChild(childIndex).gameObject;
+        }
+    }
+    void StartingPointDir()
+    {
+        patrolWP = Random.Range(0, waypoints.Length - 1);
+
+        int alea = Random.Range(0, 2);
+        if (alea % 2 == 0)
+        {
+            backwards = true;
+        }
+        else
+        {
+            backwards = false;
         }
     }
 
